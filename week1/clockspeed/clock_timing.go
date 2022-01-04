c3a5f3341dba9af9bccd2462d366ff8ba944dfee
andrewdoss <andrewjdoss@gmail.com>
2022-01-03 20:19:32 -0800

    Add other type of benchmark

--- week1/clockspeed/clock_timing.go
+++ week1/clockspeed/clock_timing.go
@@ -1,6 +1,26 @@
 package clockspeed
 
+import (
+	"fmt"
+	"time"
+)
+
 const iter = 1000000000
+const trials = 10
+const factor = 1.001
+
+func ClockSpeed() {
+	total := 0.0
+	for i := 0; i < trials; i++ {
+		start := time.Now()
+		testLoop()
+		elapsed := time.Since(start)         // ns
+		frequency := iter / float64(elapsed) // GHz
+		total += frequency
+		fmt.Printf("Estimated clock-speed is %4.2f GHz.\n", frequency)
+	}
+	fmt.Printf("Average estimate over %d trials is %4.2f GHz.", trials, total/trials)
+}
 
 func testLoop() {
 	for i := 0; i < iter; i++ {
@@ -15,18 +35,18 @@ func testLoopAdd() int {
 	return x
 }
 
-func testLoopMult() int {
-	x := 0
+func testLoopMult() float64 {
+	x := 0.0
 	for i := 0; i < iter; i++ {
-		x = i * 146
+		x *= factor
 	}
 	return x
 }
 
-func testLoopDivide() int {
-	x := 0
+func testLoopDivide() float64 {
+	x := 0.0
 	for i := 0; i < iter; i++ {
-		x = i / 146
+		x /= factor
 	}
 	return x
 }
---
+++ week1/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "ssbs/exercises/week1/clockspeed"
+
+func main() {
+	clockspeed.ClockSpeed()
+}
